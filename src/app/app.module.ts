bc3d1425bb2a421251e458e456e666ed4510d93c
dzonib <nikolabosnjak381@gmail.com>
2018-09-04 14:32:15 +0200

    second commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { Grid1Component } from './grid/grid1/grid1.component';
+import { Grid2Component } from './grid/grid2/grid2.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    Grid1Component
+    Grid1Component,
+    Grid2Component
   ],
   imports: [
     BrowserModule,
